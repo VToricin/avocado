d50e6329cfc070fb459904225980e3463f152859
whiitered <whiitered@gmail.com>
2022-02-22 16:22:42 +0300

    html without react

---
+++ index.js
@@ -0,0 +1,30 @@
+let avocado  = document.getElementById('avocado');
+let mainBlockMoto = document.querySelector('.mainBlockMoto');
+let windowHeight = window.innerHeight;
+let avocadoTop = 0;
+
+function avocadoPosition () {
+windowHeight = window.innerHeight;
+if (windowHeight<420) {
+   avocadoTop = -40;
+} else if (windowHeight<680) {
+   avocadoTop = 250;   
+} else {
+   avocadoTop = 320;
+} 
+
+avocado.style.top = `${avocadoTop}px`;
+}
+avocadoPosition();
+
+window.addEventListener('scroll',()=>{
+    
+  avocado.style.top = `${avocadoTop-window.pageYOffset}px`;
+  if (window.pageYOffset>400) {
+     mainBlockMoto.style.color = "black"; 
+  }else {   
+     mainBlockMoto.style.color = "rgb(187, 187, 187)"; 
+  }
+})
+
+window.addEventListener('resize', avocadoPosition);
